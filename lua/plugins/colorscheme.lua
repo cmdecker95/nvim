cae0b9e2296cf50a08fa7a7a4a3b42ec1ac5ec9f
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2024-01-28 19:22:34 -1000

    Update colorscheme config
    - lua/plugins/colorscheme.lua is now a list of color schemes to import
    - the default colorscheme is set in init.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,8 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
-  { "EdenEast/nightfox.nvim" },
-  { "rose-pine/neovim", name = "rose-pine" },
-  { "craftzdog/solarized-osaka.nvim" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "terafox",
-    },
-  },
+	{ "folke/tokyonight.nvim" },
+	{ "navarasu/onedark.nvim" },
+	{ "ellisonleao/gruvbox.nvim" },
+	{ "EdenEast/nightfox.nvim" },
+	{ "rose-pine/neovim" },
+	{ "craftzdog/solarized-osaka.nvim" },
 }
