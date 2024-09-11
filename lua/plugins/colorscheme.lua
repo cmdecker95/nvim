f4d53bbcd9736584bcd901924bb13cdf84e059bb
Christian Decker <decker_christian@bah.com>
2024-09-11 13:00:10 -0400

    Add catppuccin theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,10 @@
 return {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	{ "folke/tokyonight.nvim" },
 	{ "navarasu/onedark.nvim" },
 	{ "ellisonleao/gruvbox.nvim" },
 	{ "EdenEast/nightfox.nvim" },
 	{ "rose-pine/neovim" },
 	{ "craftzdog/solarized-osaka.nvim" },
-  { "ayu-theme/ayu-vim" }
+	{ "ayu-theme/ayu-vim" },
 }
