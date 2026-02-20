96bdfc95f02bb83ab10880334aac9ea6a2141ab2
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2026-02-20 12:16:17 -0500

    Properly disable inlay hints

--- init.lua
+++ init.lua
@@ -1,8 +1,5 @@
 -- Initialize and setup lazy.nvim
 require("config.lazy")
 
--- Set default colorscheme
+-- Set default colorscheme/theme
 vim.cmd.colorscheme("kanagawa-dragon")
-
--- Disable inlay hints
-vim.lsp.inlay_hint.enable(false)
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,9 @@
 return {
 	"neovim/nvim-lspconfig",
 	opts = {
+		inlay_hints = {
+			enabled = false, -- turn off inlay hints
+		},
 		servers = {
 			lua_ls = {
 				settings = {
