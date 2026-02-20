0ded6544a7d07613a0272b83c064753fac359219
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2026-02-20 12:16:36 -0500

    Fix LSP errors in config.lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,6 +1,6 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
 		"clone",
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 	})
 end
 
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	spec = {
