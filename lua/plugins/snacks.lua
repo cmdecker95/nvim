cac9e294e103654d8ce2209d3bb564254f829322
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2026-05-29 14:58:19 -0400

    add snacks.lua
    configure options for snacks explorer:
    - show hidden files by default

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		-- Enable showing hidden files,
+		picker = {
+			sources = {
+				explorer = {
+					hidden = true,
+				},
+			},
+		},
+	},
+}
