d29953e65d83b3b0082ad6d62f59b16029053e92
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2026-06-17 10:32:37 -0400

    add nice autocmds and keymaps

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,2 +1,9 @@
 -- Autocmds are automatically loaded from this file on the VeryLazy event.
 -- LazyVim provides defaults in lazyvim.config.autocmds; add your own below.
+
+-- Disable auto-comment continuation
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,9 @@
 
 local map = vim.keymap.set
 
--- Save with <leader>w
-map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
+-- Keep cursor centered on half-page jumps
+map("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+
+-- Paste over a visual selection without clobbering the yank register
+map("x", "p", [["_dP]], { desc = "Paste without yanking selection" })
