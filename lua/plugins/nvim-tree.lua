281eef1931ba9188989adad0d11db4ceee2bcb64
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2026-09-18 15:09:51 -0400

    add convenience binds to explorer

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -10,6 +10,24 @@ return {
 	},
 	opts = {
 		hijack_directories = { enable = false },
+		on_attach = function(bufnr)
+			local api = require("nvim-tree.api")
+
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
+
+			api.config.mappings.default_on_attach(bufnr)
+
+			vim.keymap.set("n", ".", function()
+				local node = api.tree.get_node_under_cursor()
+				if node and node.nodes then
+					api.tree.change_root_to_node(node)
+				end
+			end, opts("CD"))
+
+			vim.keymap.set("n", "<BS>", api.tree.change_root_to_parent, opts("Up"))
+		end,
 		filters = {
 			custom = { "^\\.git$" },
 		},
